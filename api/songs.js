7164d195d411a65eba7f79a52270fecd813ba00a
work-ham <ilhamahmadre@gmail.com>
2024-10-02 14:38:55 +0700

    update for deploy to vercel

--- api/songs.js
+++ api/songs.js
@@ -1,20 +1,21 @@
-// api/songs.js
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
-module.exports = async (req, res) => {
-    const musicDir = path.join(__dirname, '../public/music');
-    
-    fs.readdir(musicDir, (err, files) => {
-        if (err) {
-            return res.status(500).json({ message: 'Unable to scan directory' });
-        }
+export default function handler(req, res) {
+  const musicDir = path.join(process.cwd(), 'public', 'music');
 
-        const songs = files.filter(file => file.endsWith('.mp3')).map(file => ({
-            title: file.replace('.mp3', ''),  // Remove file extension for title
-            src: `/music/${file}`  // Relative path to music files
-        }));
+  // Read the music directory and list mp3 files
+  fs.readdir(musicDir, (err, files) => {
+    if (err) {
+      return res.status(500).json({ message: 'Unable to scan directory' });
+    }
 
-        res.status(200).json(songs);  // Return song list
-    });
-};
+    // Filter mp3 files and create a list of song objects
+    const songs = files.filter(file => file.endsWith('.mp3')).map(file => ({
+      title: file.replace('.mp3', ''),
+      src: `/music/${file}`  // Ensure the correct path for the client to access
+    }));
+
+    res.status(200).json(songs);  // Send list of songs to the frontend
+  });
+}
